3eefc315d5d62ec6c66500c964f6826e9d40c06d
Mr.Kim <joajy98@gmail.com>
2023-11-11 17:14:06 +0900

    feat: 혜택으로 인한 할인 내역을 출력하는 메서드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,9 @@
 package christmas.view;
 
 import java.text.DecimalFormat;
+import java.util.List;
+
+import static christmas.util.OutputConstant.*;
 
 public class OutputView {
 
@@ -8,4 +11,39 @@ public class OutputView {
         DecimalFormat df = new DecimalFormat("###,###");
         return df.format(money);
     }
+
+    public static void printBenefit(List<Integer> discounts, int money) {
+        printChristmasBenefit(discounts.get(0));
+        printSpecialBenefit(discounts.get(1));
+        printWeekdayBenefit(discounts.get(2));
+        printWeekendBenefit(discounts.get(3));
+        printFreebieBenefit(money);
+    }
+
+    public static void printChristmasBenefit(int money){
+        if(money == 0) return;
+        System.out.println(CHRISTMAS_BENEFIT + "-" + splitMoneyView(money) + PRICE_UNIT);
+    }
+
+    public static void printWeekdayBenefit(int money) {
+        if(money == 0) return;
+        System.out.println(WEEKDAY_BENEFIT + "-" + splitMoneyView(money) + PRICE_UNIT);
+    }
+
+    public static void printWeekendBenefit(int money) {
+        if(money == 0) return;
+        System.out.println(WEEKEND_BENEFIT + "-" + splitMoneyView(money) + PRICE_UNIT);
+    }
+
+    public static void printSpecialBenefit(int money) {
+        if(money == 0) return;
+        System.out.println(SPECIAL_BENEFIT + "-"  + splitMoneyView(money) + PRICE_UNIT);
+    }
+
+    public static void printFreebieBenefit(int money) {
+        if(money >= 120000){
+            System.out.println(FREEBIE_BENEFIT);
+        }
+    }
+
 }
