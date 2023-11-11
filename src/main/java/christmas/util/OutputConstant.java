d99f039a1d4d129c74d0c642ad9aa93350fb0485
Mr.Kim <joajy98@gmail.com>
2023-11-11 11:49:09 +0900

    feat: view 패키지 내 클래스의 가독성을 위해 상수 메시지를 별도로 util 패키지에 생성

---
+++ src/main/java/christmas/util/OutputConstant.java
@@ -0,0 +1,26 @@
+package christmas.util;
+
+public class OutputConstant {
+
+    public static final String ORDER_MENU = "<주문 메뉴>";
+    public static final String FREE_MENU = "<증정 메뉴>";
+    public static final String BENEFITS_HISTORY = "<혜택 내역>";
+    public static final String FREE_GIFT = "샴페인 1개\n";
+    public static final String BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String CHRISTMAS_BENEFIT = "크리스마스 디데이 할인: ";
+    public static final String WEEKDAY_BENEFIT = "평일 할인: ";
+    public static final String WEEKEND_BENEFIT = "주말 할인: ";
+    public static final String SPECIAL_BENEFIT = "특별 할인: ";
+    public static final String FREEBIE_BENEFIT = "증정 이벤트: -25,000원\n";
+    public static final String DISCOUNT_BENEFIT = "<총혜택 금액>";
+    public static final String AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+    public static final String PRICE_UNIT = "원";
+    public static final String VVIP_BADGE = "산타";
+    public static final String VIP_BADGE = "트리";
+    public static final String NORMAL_BADGE = "별";
+    public static final String NOTHING = "없음";
+    public static final int VVIP_PRICE = 20000;
+    public static final int VIP_PRICE = 10000;
+    public static final int NORMAL_PRICE = 5000;
+}
