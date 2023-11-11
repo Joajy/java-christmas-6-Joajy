385bcd3453b95201b49047cf7f9f23aab5656e4e
Mr.Kim <joajy98@gmail.com>
2023-11-11 11:53:59 +0900

    feat: 주문 시 발생할 수 있는 문제에 대한 예외 처리

---
+++ src/main/java/christmas/validator/OrderValidator.java
@@ -0,0 +1,56 @@
+package christmas.validator;
+
+import christmas.domain.Order;
+import christmas.domain.menu.MenuList;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class OrderValidator {
+
+    private static final int MAX_MENU_COUNT = 20;
+    private static final int MIN_MENU_COUNT = 1;
+
+    public static void validateOrderStatus(List<Order> orders) {
+        validateOrderCount(orders);
+        validateOrderOnlyBeverage(orders);
+        validateDuplicateOrder(orders);
+    }
+
+    public static void validateDuplicateOrder(List<Order> orders) {
+        HashSet<String> menuName = new HashSet<>();
+        for (Order order : orders) {
+            menuName.add(order.getMenuName());
+        }
+        if(menuName.size() != orders.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateOrderOnlyBeverage(List<Order> orders) {
+        int orderBeverage = 0;
+        for (Order order : orders) {
+            orderBeverage += validateOrderIsBeverage(order);
+        }
+        if(orderBeverage == orders.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int validateOrderIsBeverage(Order order) {
+        if(MenuList.BEVERAGE.contains(order.getMenuName())) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void validateOrderCount(List<Order> orders) {
+        int orderCount = 0;
+        for (Order order : orders) {
+            orderCount += order.getOrderAmount();
+        }
+        if(orderCount > MAX_MENU_COUNT || orderCount < MIN_MENU_COUNT){
+            throw new IllegalArgumentException();
+        }
+    }
+}
