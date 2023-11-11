f5ba2e70273bdcf8bd517db7b9ddf7c95635700b
Mr.Kim <joajy98@gmail.com>
2023-11-11 11:35:29 +0900

    feat: 메뉴 카테고리를 쉽게 찾을 수 있도록 카테고리별 메뉴 문자열 생성

---
+++ src/main/java/christmas/domain/menu/MenuList.java
@@ -0,0 +1,9 @@
+package christmas.domain.menu;
+
+public class MenuList {
+
+    public static final String APPETIZER = "양송이수프, 타파스, 시저샐러드";
+    public static final String MAIN = "티본스테이크, 바비큐립, 해산물파스타, 크리스마스파스타";
+    public static final String DESSERT = "초코케이크, 아이스크림";
+    public static final String BEVERAGE = "제로콜라, 레드와인, 샴페인";
+}
